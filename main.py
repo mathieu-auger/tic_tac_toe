4defdd41120206e536cef076e1db98b3bba85188
Elaf Al Nossirat <elaf.al-nossirat@laplateforme.io>
2024-12-04 11:38:38 +0100

    print board to show the last entry before winning

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# import random
 
 # adding global variabels
 
@@ -36,34 +35,41 @@ def checkHorizontle(board):
     if board[0] == board[1] == board[2] and board[0] != "-":
       winner = board[0]
       return True
+      
     elif board[3] == board[4] == board[5] and board[3] != "-":
       winner = board[3]
       return True 
+      
     elif board[6] == board[7] == board[8] and board[6] != "-":
       winner = board[6]
       return True 
+      
     
 def checkRow(board):
     global winner
     if board[0] == board[3] == board[6] and board[0] != "-":
       winner = board[0]
       return True
+      
     elif board[1] == board[4] == board[7] and board[1] != "-":
       winner = board[1]
       return True
+      
     elif board[2] == board[5] == board[8] and board[2] != "-":
       winner = board[2]
       return True
+      
     
 def checkDiag(board):
   global winner
   if board[0] == board[4] == board[8] and board[0] != "-":
       winner = board[0]
-      return True    
+      return True
+          
   elif board[2] == board[4] == board[6] and board[2] != "-":
       winner = board[2]
       return True     
-
+      
 def checkTie(board):
   global gameRunning
   if "-"  not in board:
@@ -73,9 +79,11 @@ def checkTie(board):
     
     
 def checkWin():
+    global winner
     if checkDiag(board) or checkHorizontle(board) or checkRow(board):
+      printBoard(board)
       print(f"the winner is {winner}")  
-      exit()  
+      exit()            
       
       
 # switch the player  
@@ -86,13 +94,6 @@ def switchPlayer():
   else:
     currentPlayer = "X"
     
-# computer
-# def computer(board):
-#     while currentPlayer == "O":
-#       position = random.randint(0,8)
-#       if board[position] == "-":
-#         board[position] = "O"
-#         switchPlayer()
            
     
 while gameRunning:
@@ -101,7 +102,6 @@ while gameRunning:
   checkWin()
   checkTie(board)
   switchPlayer()
-  # computer(board)     
   checkWin()
   checkTie(board)          
   
